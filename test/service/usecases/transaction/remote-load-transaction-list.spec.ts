d6d62c4c7d43f25a56dc8d107deb8da74896f71a
Felipe Pedrosa <marconizero@gmail.com>
2023-03-20 16:37:22 -0300

    refactor: change RemoteLoadTransactionList to use faker

--- test/service/usecases/transaction/remote-load-transaction-list.spec.ts
+++ test/service/usecases/transaction/remote-load-transaction-list.spec.ts
@@ -1,13 +1,14 @@
 import { HttpGetClient } from '../../../../src/service/protocols/api/http-get-client'
 import { RemoteLoadTransactionList } from '../../../../src/service/usecases/transaction/remote-load-transaction-list'
 import { HttpGetClientSpy } from '../../mocks/mock-http-client'
+import faker from 'faker'
 
 type SubTypes = {
   sut: RemoteLoadTransactionList
   httpGetClientSpy: HttpGetClient
 }
 
-const makeSut = (url = 'any_url'): SubTypes => {
+const makeSut = (url: string = faker.internet.url()): SubTypes => {
   const httpGetClientSpy = new HttpGetClientSpy()
   const sut = new RemoteLoadTransactionList(url, httpGetClientSpy)
   return {
@@ -18,8 +19,8 @@ const makeSut = (url = 'any_url'): SubTypes => {
 
 describe('RemoteLoadTransactionList', () => {
   test('should call HttpGetClient with correct URL', async () => {
-    const url = 'any_url'
-    const { sut, httpGetClientSpy } = makeSut()
+    const url = faker.internet.url()
+    const { sut, httpGetClientSpy } = makeSut(url)
     await sut.loadTransanctionList()
     expect(httpGetClientSpy.getUrl()).toBe(url)
   })
