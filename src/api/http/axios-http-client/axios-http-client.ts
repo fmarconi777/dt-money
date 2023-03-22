3a6ca264bc08af96ee4ded6246e89d0731597667
Felipe Pedrosa <marconizero@gmail.com>
2023-03-22 15:58:50 -0300

    feat: ensure AxiosHttpClient returns correct values

--- src/api/http/axios-http-client/axios-http-client.ts
+++ src/api/http/axios-http-client/axios-http-client.ts
@@ -1,12 +1,12 @@
-import { HttpGetClient, HttpGetParams, HttpResponse, HttpStatusCode } from '@/service/protocols/api/http'
+import { HttpGetClient, HttpGetParams, HttpResponse } from '@/service/protocols/api/http'
 import axios from 'axios'
 
-export class AxiosHttpClient implements HttpGetClient<HttpGetParams<any>, any> {
+export class AxiosHttpClient implements HttpGetClient<any, any> {
   async get (params: HttpGetParams<any>): Promise<HttpResponse<any>> {
-    await axios.get(params.url)
+    const httpResponse = await axios.get(params.url)
     return {
-      statusCode: HttpStatusCode.ok,
-      body: ''
+      statusCode: httpResponse.status,
+      body: httpResponse.data
     }
   }
 }
